8863ecf5ec0744ca4ea3f6a11ea91c1071d9949a
mendy hershkop <hers.mm@gmail.com>
2025-12-17 10:42:04 +0200

    create search by age function

---
+++ utils/searchByAge.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+export default function searchByAge(age){
+    const lists =fs.readFileSync('data/PEOPLE.json', 'UTF-8',(err, data)=>{if (err) throw err; console.log(data)})
+    const jlist =JSON.parse(lists)
+    // console.log(jlist);
+    let nameList = []
+    jlist.forEach(person => {
+        if (person.age == age){
+            nameList.push(person)
+        }})
+        return new Promise((res, rej) =>{
+            if (nameList.length>=1){
+            res(nameList)
+            }
+        else {
+            rej("No settings were found that match your search. Try something else.")
+        }
+    });
+   
+}
+searchByAge(54).then((msg)=>console.log(msg)).catch((msg)=>console.log(msg))
